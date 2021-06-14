51032d3f97ee596853222f593d91e3ead2174a25
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-06-13 19:02:25 -0500

    completando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from "@angular/router";
 import {HomeComponent} from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SeccionesComponent } from './components/secciones/secciones.component';
 
 const APP_ROUTES: Routes = [
     {path:'home', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'secciones', component: SeccionesComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
