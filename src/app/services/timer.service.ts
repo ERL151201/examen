2148daddf310550c7064cbba6e229e546bc59a04
Luis Angel <santosangel748@gmail.com>
2021-08-20 15:55:07 -0500

    Pequeños cambios visuales

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -8,7 +8,9 @@ import Swal from 'sweetalert2';
 })
 export class TimerService {
 
-  timeLeft: number = 600;//Duracion del examen (Modificar en segundos)
+  timeLeft: number = 600;//Duracion del examen (Modificar en segundos) 
+  //60 = 1 minuto, 600 = 10 minutos, 3600 = 1 hora(60 minutos)   
+  
   interval: any;
   interval2: any;
   horas: any;
